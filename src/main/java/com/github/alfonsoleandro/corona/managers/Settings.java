751cab09e938853eb9a1f714bff663ce48485230
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2022-06-08 13:11:21 -0300

    Update Settings.java
    Added a line I forgot to add. Causing NullPointerExceptions

--- src/main/java/com/github/alfonsoleandro/corona/managers/Settings.java
+++ src/main/java/com/github/alfonsoleandro/corona/managers/Settings.java
@@ -56,6 +56,7 @@ public class Settings extends Reloadable {
     public Settings(Corona plugin) {
         super(plugin);
         this.plugin = plugin;
+        loadFields();
     }
 
     private void loadFields() {
